59c1f84a60ade9d6c4eb12f7f29ec410182d2d5b
DigitalTables <LioDigitalTables@gmail.com>
2025-10-23 22:43:01 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,8 +4,8 @@ import Papa from "https://cdn.jsdelivr.net/npm/papaparse@5.4.1/+esm";
 console.log("✅ Fichier supabase.js chargé");
 
 const supabaseUrl = "https://xrffjwulhrydrhlvuhlj.supabase.co";
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyZmZqd3VsaHJ5ZHJobHZ1aGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc2MDQsImV4cCI6MjA3NjIwMzYwNH0.uzlCCfMol_8RqRG2fx4RITkLTZogIKWTQd5zhZELjhg';
+const supabase = createClient(supabaseUrl, supabaseKey)
 window.supabase = supabase;
 
 // --- Sélecteurs DOM ---
