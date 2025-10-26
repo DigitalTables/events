e03585b71bc483b2b1d51dabe84ea4b444313ef4
DigitalTables <LioDigitalTables@gmail.com>
2025-10-27 00:14:22 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.1";
 import Papa from "https://cdn.jsdelivr.net/npm/papaparse@5.4.1/+esm";
 console.log("✅ supabase.js chargé");
 export const SUPABASE_URL = "https://xrffjwulhrydrhlvuhlj.supabase.co";
-export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyZmZqd3VsaHJ5ZHJobHZ1aGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc2MDQsImV4cCI6MjA3NjIwMzYwNH0.uzlCCfMol_8RqRG2fx4RITkLTZogIKWTQd5zhZELjhg";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // --- Détection automatique du séparateur CSV ---
