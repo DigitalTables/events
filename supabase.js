85d39fde06b2bfbeca7b6720c878c026045a1847
DigitalTables <LioDigitalTables@gmail.com>
2025-10-27 15:18:56 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@ console.log("✅ supabase.js chargé");
 // ⚠️ Remplace avec tes vraies infos projet
 const supabaseUrl = "https://xrffjwulhrydrhlvuhlj.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyZmZqd3VsaHJ5ZHJobHZ1aGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc2MDQsImV4cCI6MjA3NjIwMzYwNH0.uzlCCfMol_8RqRG2fx4RITkLTZogIKWTQd5zhZELjhg";
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Fonction RGPD-friendly pour obtenir le CSV privé via la Edge Function
 export async function getPrivateCsv(username, password, userId) {
