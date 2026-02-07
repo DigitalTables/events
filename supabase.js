6dc14fcc41518775f0898cd5715903b5ea80cc7a
DigitalTables <LioDigitalTables@gmail.com>
2026-02-08 00:55:09 +0100

    Refactor Supabase client setup with auth options
    Updated Supabase client initialization with new anon key and added authentication options.

--- supabase.js
+++ supabase.js
@@ -1,9 +1,17 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.1";
 
 export const SUPABASE_URL = "https://xrffjwulhrydrhlvuhlj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyZmZqd3VsaHJ5ZHJobHZ1aGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc2MDQsImV4cCI6MjA3NjIwMzYwNH0.uzlCCfMol_8RqRG2fx4RITkLTZogIKWTQd5zhZELjhg";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyZmZqd3VsaHJ5ZHJobHZ1aGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc2MDQsImV4cCI6MjA3NjIwMzYwNH0.uzlCCfMol_8RqRG2fx4RITkLTZogIKWTQd5zhZELjhg";
 
-export const supabase = createClient(SUPABASE_URL, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    // ✅ IMPORTANT : Remplacez par votre vraie URL GitHub Pages
+    redirectTo: 'https://digitaltables.github.io/events/index.html',
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+});
 export const GOOGLE_CLIENT_ID= "1036214240835-f3cpd0sjeomk1gc31t3sv6v4r6shl729.apps.googleusercontent.com"
 /**
  * Récupère le CSV privé signé via Edge Function
